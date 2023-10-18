66cd5c1973089771df2a3bc2f3dbf5adbeb7d5d5
Bùi Hải Triều <trieuql159@gmail.com>
2023-10-18 15:22:31 +0700

    update

---
+++ MotorbikeRental/BusinessLogicLayer/khacNgocBaiHocRutRa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorbikeRental.BusinessLogicLayer
+{
+    internal class khacNgocBaiHocRutRa
+    {
+     /*   Biết cách sử dụng hệ thông svn , biết cách sử dụng github trong dự án*/
+    }
+}
