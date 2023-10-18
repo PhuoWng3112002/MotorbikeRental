4447bb5567cea6fca2047a127d8663b5e4e0add4
Bùi Hải Triều <trieuql159@gmail.com>
2023-10-18 15:02:00 +0700

    pull request hai trieu

---
+++ MotorbikeRental/BusinessLogicLayer/haitrieuBaiHocRutRa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorbikeRental.BusinessLogicLayer
+{
+    internal class haitrieuBaiHocRutRa
+    {
+       /*  Biết cách sử dụng một chương trình quản lý SVN . Nhóm em sử dụng github .
+       Phải biết cách sử dụng github trong dự án của mình
+        */
+    }
+}
