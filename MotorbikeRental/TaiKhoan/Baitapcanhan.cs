82d092e32a70cbaf670f08611fdd872386796af1
TrangDuong114 <duongtranga4ltt@gmail.com>
2023-10-18 15:23:27 +0700

    Bài tập ngày 18/10/2023

---
+++ MotorbikeRental/TaiKhoan/Baitapcanhan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorbikeRental.TaiKhoan
+{
+    class Baitapcanhan
+    {
+        /*Những gì rút ra trong hôm nay
+          - Phải biết sử dụng 1 công cụ SVN
+          - Biết tạo new branch; push, pull request, merge, rollback
+
+
+      */
+    }
+}
