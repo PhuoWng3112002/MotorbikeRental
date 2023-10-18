94401b41124d5c9b2d7cc9af8cc26a4f699bfcf6
phuong <thphg311@gmail.com>
2023-10-18 15:55:54 +0700

    GTH

---
+++ MotorbikeRental/GiangTheHiep.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorbikeRental
+{
+    class GiangTheHiep
+    {
+        //rút  ra bài học về git, github 
+        //tạo branch mới mới, sử dụng lệnh add, push, pull, request, merge 2 branch, revert/rollback
+    }
+}
